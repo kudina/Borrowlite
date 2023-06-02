dc3bfb02fc388490a753b9f676e9054811a85c0b
Uhembenelson <uhembenelson@gmail.com>
2023-06-02 10:45:09 +0100

    cors

--- index.js
+++ index.js
@@ -4,17 +4,17 @@ import { MONGO_URL } from './config/index.js';
 import mongoose from 'mongoose';
 import cors from 'cors';
 const port = process.env.PORT || 5000;
-const corsOptions ={
-  origin: 'https://borrowlite.com/',
- //origin:'http://localhost:3000', 
-  credentials:true,            //access-control-allow-credentials:true
-  optionSuccessStatus:200
-}
+// const corsOptions ={
+//   origin: 'https://borrowlite.com/',
+//  //origin:'http://localhost:3000', 
+//   credentials:true,            //access-control-allow-credentials:true
+//   optionSuccessStatus:200
+// }
 
 const app = express();
 app.use(bodyParser.json()); 
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cors(corsOptions));
+app.use(cors());
 
 // app.use(function(req, res, next) {
 //   res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // Update to match the domain you will make the request from
